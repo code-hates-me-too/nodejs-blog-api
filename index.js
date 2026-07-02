ea13d6bc98f413a627bb58f93f4fa51c8bbe13be
fantaste-liquidity <area.fatihdogan@gmail.com>
2026-07-02 23:27:05 +0300

    feat: MySQL veritabanı bağlantısı yapılandırıldı

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
+const mysql = require("mysql2");
+const config = require("./config");
+
+let connection = mysql.createConnection(config.db);
+
+connection.connect((err) => {
+    if(err){
+        return console.log(err);
+    }
+    console.log("mysql server bağlantısı kuruldu");
+});
 
 const app = express();
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 app.use("/libs", express.static(path.join(__dirname, "node_modules")));
 app.use("/static", express.static(path.join(__dirname, "public")));
