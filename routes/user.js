77898f02af9ef2202f8fe6a1e4f247460b71e69a
fantaste-liquidity <area.fatihdogan@gmail.com>
2026-07-04 22:57:08 +0300

    feat: kategori filtreleme ve blog detay sayfası eklendi

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,47 @@ const path = require("path");
 const router = express.Router();
 const db = require("../data/db");
 
-router.get("/blogs/:blogid", (req, res) => {
-    res.render("users/blog-details");
+router.get("/blogs/category/:categoryid", async (req, res) => {
+    const categoryid = req.params.categoryid;
+    try {
+        const [blogs] = await db.execute(
+            "SELECT * FROM blog WHERE categoryid=?", 
+            [categoryid]
+        );
+
+        const [categories] = await db.execute(
+            "SELECT * FROM category"
+        );
+
+        const  [title] = await db.execute("SELECT categoryname FROM category WHERE categoryid=?", [categoryid]); 
+        res.render("users/blogs", {
+            title: title[0].categoryname,
+            blogs,
+            categories,
+            selectedCategory: categoryid
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+router.get("/blogs/:blogid", async (req, res) => {
+    const blogid = req.params.blogid;
+    
+    try {
+        const [blog, ] = await db.execute("SELECT * FROM blog WHERE blogid=?", [blogid]);
+        if(blog[0]) {
+            return res.render("users/blog-details", {
+                title: blog[0].baslik,
+                blog: blog[0],
+                selectedCategory: null
+            });
+        } 
+        res.redirect("/blogs");
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 router.get("/blogs", async (req, res) => {
@@ -20,7 +59,8 @@ router.get("/blogs", async (req, res) => {
         res.render("users/blogs", {
             title: "Tüm Bloglar",
             blogs,
-            categories
+            categories,
+            selectedCategory: null
         });
     } catch (err) {
         console.log(err);
@@ -40,7 +80,8 @@ router.get("/", async (req, res) => {
         res.render("users/index", {
             title: "Anasayfa",
             blogs,
-            categories
+            categories,
+            selectedCategory: null
         });
 
     } catch (err) {
