66f0ad4a689de6b257f6f0932956ccdb9c7534db
fantaste-liquidity <area.fatihdogan@gmail.com>
2026-07-10 21:51:54 +0300

    feat: admin blog düzenleme sayfası ve güncelleme işlemi eklendi

--- routes/admin.js
+++ routes/admin.js
@@ -30,12 +30,62 @@ router.post("/blogs/create", async (req, res) => {
         VALUES (?,?,?,?,?,?)`, [baslik, aciklama, resim, anasayfa, onay, kategori]);
         res.redirect("/");
     } catch (err) {
-        console.write(err);
+        console.log(err);
     }
 });
 
-router.get("/blogs", (req, res) => {
-    res.render("admins/blog-list", {title: "Edit Blogs"});
+router.get("/blogs/:blogid", async (req, res) => {
+    const pickedblogid = req.params.blogid;
+
+    try {
+        const [blogs, ] = await db.execute("SELECT * FROM blog WHERE blogid=?", [pickedblogid]);
+        const [categories, ] = await db.execute("SELECT * FROM category");
+        const blog = blogs[0];
+        
+        if(blog) {
+            return res.render("admins/blog-edit", {
+                title: blog.baslik + "Edit",
+                blog: blog,
+                categories: categories
+            });
+        }
+
+        res.redirect("/admin/blogs");
+
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.post("/blogs/:blogid", async (req, res) => {
+    const blogid = req.body.blogid;
+    const baslik = req.body.baslik;
+    const aciklama = req.body.aciklama;
+    const resim = req.body.resim;
+    const anasayfa = req.body.anasayfa == "on" ? 1 : 0;
+    const onay = req.body.onay == "on" ? 1 : 0;
+    const kategori = req.body.kategori;
+
+    try {
+        await db.execute("UPDATE blog SET baslik=?, aciklama=?, resim=?, anasayfa=?, onay=?, categoryid=? WHERE blogid=?",
+        [baslik, aciklama, resim, anasayfa, onay, kategori, blogid]);
+        res.redirect("/admin/blogs");
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.get("/blogs", async (req, res) => {
+    try {
+        const [blogs, ] = await db.execute("SELECT * FROM blog");
+        
+        res.render("admins/blog-list", {
+            title: "Edit Blogs",
+            blogs: blogs
+        });
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 module.exports = router;
