90a4cb8807c44948ae5a1ccbad899c96b57c06a1
fantaste-liquidity <area.fatihdogan@gmail.com>
2026-07-16 17:17:39 +0300

    feat: kategorileri yönetme işlevleri eklendi

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,102 @@ const path = require("path");
 const router = express.Router();
 const db = require("../data/db");
 
+router.get("/categories/delete/:categoryid", async (req, res) => {
+    const delcategoryid = req.params.categoryid;
+    try {
+        const [category, ] = await db.execute("SELECT * FROM category WHERE categoryid=?", [delcategoryid]);
+        res.render("admins/category-delete", {
+            title: "Delete Category",
+            category: category[0]
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.post("/categories/delete/:categoryid", async (req, res) => {
+    const delcategoryid = req.body.categoryid;
+    try {
+        await db.execute("DELETE FROM category WHERE categoryid=?", [delcategoryid]);
+        res.redirect("/admin/categories?action=delete");
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.get("/categories/create", async (req, res) => {
+    try {
+        const [categories, ] = await db.execute("SELECT * FROM category") 
+        
+        res.render("admins/category-create", {
+            title: "Create Category",
+            categories: categories
+        });
+
+    } catch (err) {
+        console.write(err);
+    }
+});
+
+router.post("/categories/create", async (req, res) => {
+    const baslik = req.body.baslik;
+
+    try {
+        await db.execute(`INSERT INTO category(categoryname)
+        VALUES (?)`, [baslik]);
+        res.redirect("/admin/categories?action=create");
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.get("/categories/:categoryid", async (req, res) => {
+    const pickedcategoryid = req.params.categoryid;
+
+    try {
+        const [category, ] = await db.execute("SELECT * FROM category WHERE categoryid=?", [pickedcategoryid]);
+        
+        if(category) {
+            return res.render("admins/category-edit", {
+                title: " Kategori Edit",
+                category: category[0]
+            });
+        }
+
+        res.redirect("/admin/categories");
+
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.post("/categories/:categoryid", async (req, res) => {
+    const categoryid = req.body.categoryid;
+    const baslik = req.body.baslik;
+
+    try {
+        await db.execute("UPDATE category SET categoryname=? WHERE categoryid=?",
+        [baslik, categoryid]);
+        res.redirect("/admin/categories?action=edit");
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.get("/categories", async (req, res) => {
+    try {
+        const [categories, ] = await db.execute("SELECT * FROM category");
+        
+        res.render("admins/category-list", {
+            title: "Edit Categories",
+            categories: categories,
+            action: req.query.action
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
 router.get("/blog/delete/:blogid", async (req, res) => {
     const delblogid = req.params.blogid;
     try {
